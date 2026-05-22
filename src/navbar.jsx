ecd60f6a021dc149573ad5247f69e7ada54aaf64
codingkingkong90 <zseymour66@gmail.com>
2026-05-22 14:44:08 +1000

    Navbar temp fix

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return(
         <nav className="navbar">
-            
+
         </nav>
     )
 } 
