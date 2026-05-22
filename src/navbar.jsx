d3953a1d0def4a05d577b6c2eb8edfb4af9f3ba7
Zach Seymour <zseymour66@gmail.com>
2026-05-22 16:34:44 +1000

    Navbar work

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Navbar = () => {
     return(
         <nav className="navbar">
-
+            <div className="logo">BloxShowcase</div> 
+            <ul className="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
         </nav>
     )
 } 
+
+export default Navbar;
